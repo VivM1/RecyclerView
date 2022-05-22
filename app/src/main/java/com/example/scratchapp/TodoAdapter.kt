2fe6b2e7b0aa3b5923847d851674ebbdf7c2cb95
VivM1 <mevivekmi112@gmail.com>
2022-05-22 12:33:23 +0530

    correct title tab shown

--- app/src/main/java/com/example/scratchapp/TodoAdapter.kt
+++ app/src/main/java/com/example/scratchapp/TodoAdapter.kt
@@ -14,14 +14,13 @@ class TodoAdapter(var todos : List<Todo>): RecyclerView.Adapter<TodoAdapter.Todo
        /* inflates the .xml file , the root layout is inflated, attachment to root is false because the recycler is to be attached and view binding is used so val bindning....*/
         //val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo,parent, false)
 
-        return TodoViewHolder(ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return TodoViewHolder(ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent,false ))
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         /* holder is used to access the views inside viewholder i.e chkcox and textview here */
-
-
-        holder.binding.tvTitle.text = todos[position].toString()
+        holder.binding.tvTitle.text = todos[position].title // earlier whn it was showing the whole to do list tab i had used .toString and
+                                                            // it returned obj as a whole but i had to only show the title of the to do in the app so i used .title as i should
         holder.binding.checkbox.isChecked = todos[position].isChecked
     }
 
